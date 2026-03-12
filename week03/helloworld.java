17215bbaf398a8962f16103623a2368737f0276e
yhukvlghju <C112181140@nkust.edu.tw>
2026-03-12 10:50:04 +0800

    0312

---
+++ week03/helloworld.java
@@ -0,0 +1,8 @@
+public class helloworld {
+    public static void main(String[] args) {
+        System.out.println("helloworld");
+    }
+    public void myfun(String[] args) {
+        System.out.println("myfun is called");
+}
+}
